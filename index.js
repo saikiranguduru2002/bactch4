930f4808407d3b351ce5d16fd3be57b4cd09570f
saikiranguduru2002 <gudurusaikiran22@gmail.com>
2025-04-09 12:27:45 +0530

    Updated welcome message in Express app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
- 
+
 app.get('/', (req, res) => {
-  res.send('Hello batch 4 from Node.js with Express!');
+  res.send('🚀 Hello Batch 4 from Express.js! Modified and ready to push.');
 });
- 
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
